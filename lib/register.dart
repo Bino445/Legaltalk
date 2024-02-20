5cff3892c145129ca8731b513dbefd3de3cd18b9
EVE <evening.11212@gmail.com>
2024-02-20 17:54:55 +0700

    version 1.1

--- lib/register.dart
+++ lib/register.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+//เหลือทำให้กดปุ่มแล้วหน้าอื่น+เงื่อนไขถ้าไม่กรอกข้อมูล+หน้านโยบาย
+
+void main() {
+  runApp(ScreenRegister());
+}
 
 class ScreenRegister extends StatefulWidget {
   const ScreenRegister({super.key});
@@ -14,25 +19,60 @@ class _ScreenRegisterState extends State<ScreenRegister> {
   TextEditingController Password = TextEditingController();
   TextEditingController CF_Password = TextEditingController();
 
+  bool passwordVisible=false; // hide password
+
+  bool isChecked = false;
+  bool isCheckboxError = false;
+  // Test Function to handle checkbox validation
+  void validateCheckbox() {
+    setState(() {
+      // Check if the checkbox is checked
+      if (isChecked) {
+        isCheckboxError = false;
+        // Checkbox is checked, perform desired actions
+      } else {
+        isCheckboxError = true;
+        // Checkbox is not checked, show error message or perform desired actions
+      }
+    });
+  }
 
   @override
+  void initState() {
+    super.initState();
+    passwordVisible = true;
+  } //password visible
+
+    @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.white,
         leading: BackButton(
           color: Colors.black,
         ),
       ),
       body: ListView(
         children: <Widget>[
+          Center(
+            child: Center(
+              child: Image(image: AssetImage('images/logotext.png'),
+                width: 200,
+                height: 200,
+              ),
+            ),
+          ),
           Card(
             color: Color(0xFF1C243C),
+            margin: EdgeInsets.only(left: 50, right: 50),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Padding(padding: const EdgeInsets.all(8.0),
-                  child: Text("สมัครสมาชิก",
-                    style: TextStyle(color: Colors.white, fontSize: 26),
+                Center(
+                  child: Padding(padding: const EdgeInsets.all(10.0),
+                    child: Text("สมัครสมาชิก",
+                      style: TextStyle(color: Colors.white, fontSize: 26),
+                    ),
                   ),
                 ),
                 Divider(),
@@ -45,7 +85,12 @@ class _ScreenRegisterState extends State<ScreenRegister> {
                         children: <Widget>[
                           Expanded(
                             child: TextFormField(
-                              decoration: InputDecoration(labelText: 'Username'),
+                              decoration: InputDecoration(labelText: 'Username',
+                              prefixIcon: Icon(Icons.person_2_rounded),
+                                //hintText: "Username",
+                                //fillColor: Colors.white,
+                                //filled: true,
+                              ),
                               style: TextStyle(color: Colors.white),
                               controller: Username,
                               validator: (value) {
@@ -56,15 +101,16 @@ class _ScreenRegisterState extends State<ScreenRegister> {
                               },
                             ),
                           ),
-                          SizedBox(width: 20.0,),
                         ],
-                      ),
+                      ),//Username
                       Row(
                         children: <Widget>[
                           Expanded(
                             child: TextFormField(
                               keyboardType: TextInputType.emailAddress,
-                              decoration: InputDecoration(labelText: 'Email'),
+                              decoration: InputDecoration(labelText: 'Email',
+                              prefixIcon: Icon(Icons.email_rounded),
+                              ),
                               style: TextStyle(color: Colors.white),
                               controller: Email,
                               validator: (value) {
@@ -76,13 +122,26 @@ class _ScreenRegisterState extends State<ScreenRegister> {
                             ),
                           ),
                         ],
-                      ),
+                      ),//Email
                       Row(
                         children: <Widget>[
                           Expanded(
                             child: TextFormField(
-                              decoration: InputDecoration(labelText: 'Password'),
-
+                              keyboardType: TextInputType.visiblePassword,
+                              obscureText: passwordVisible,
+                              decoration: InputDecoration(labelText: 'Password',
+                                prefixIcon: Icon(Icons.password_rounded),
+                                suffixIcon: IconButton(
+                                  icon: Icon(passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                  onPressed: (){
+                                    setState(() {
+                                      passwordVisible = !passwordVisible;
+                                    });
+                                  },
+                                ),
+                              ),
                               style: TextStyle(color: Colors.white),
                               controller: Password,
                               validator: (value) {
@@ -94,8 +153,38 @@ class _ScreenRegisterState extends State<ScreenRegister> {
                             ),
                           ),
                         ],
-                      ),
-
+                      ),//password
+                      Row(
+                        children: <Widget>[
+                          Expanded(
+                            child: TextFormField(
+                              keyboardType: TextInputType.visiblePassword,
+                              obscureText: passwordVisible,
+                              decoration: InputDecoration(labelText: 'Confirm Password',
+                                prefixIcon: Icon(Icons.password_rounded),
+                                suffixIcon: IconButton(
+                                  icon: Icon(passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off),
+                                  onPressed: (){
+                                    setState(() {
+                                      passwordVisible = !passwordVisible;
+                                    });
+                                  },
+                                ),
+                              ),
+                              style: TextStyle(color: Colors.white),
+                              controller: CF_Password,
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'กรุณากรอกข้อมูล';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                        ],
+                      ),//confirm password
                     ],
                   ),
                 ),
@@ -103,6 +192,42 @@ class _ScreenRegisterState extends State<ScreenRegister> {
               ],
             ),
           ),
+          ListTile(
+            title: Text('ฉันยอมรับใน "นโยบายความปลอดภัย" รวมถึงเงื่อนไขการใช้งาน'),
+            leading: Checkbox(
+              value: isChecked,
+              onChanged: (value){
+                setState(() {
+                  isChecked = value!;
+                });
+                },
+            ),
+          ),
+              if (isCheckboxError) //pls check privacy
+                Text(
+                  'กรุณากดยอมรับนโยบายความเป็นส่วนตัว และเงื่อนไขการใช้งาน!',
+                  style: TextStyle(color: Colors.red),
+                  textAlign: TextAlign.center,
+                ),
+          Center(
+              child: SizedBox(
+                  height:50, //height of button
+                  width:200, //width of button
+                  child:ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                          primary: Color(0xFFD1B06B), //background color of button
+                          elevation: 3, //elevation of button
+                          shape: RoundedRectangleBorder( //to set border radius to button
+                              borderRadius: BorderRadius.circular(50)
+                          ),
+                          padding: EdgeInsets.all(20) //content padding inside button
+                      ),
+                      onPressed: validateCheckbox,
+                      child: Text("สมัครสมาชิก",
+                        style: TextStyle(color: Colors.white),)
+                  )
+              )
+          )//regis button
          ],
       ),
     );
